34185877cb31ba0ef6b64f8caf7ad4b6db6b9786
afnan_h <upclicks2019@gmail.com>
2019-06-29 13:27:35 +0200

    Initial Commit

---
+++ My School App_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My School App_1
+//
+//  Created by Afnan on 6/29/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
